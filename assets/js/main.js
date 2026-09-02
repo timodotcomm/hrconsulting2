fcbd59b83323ed7cb8e10c5691b84e734b2e0bea
timodotcomm <timodotcomm@gmail.com>
2026-09-02 11:40:00 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',()=>{const b=document.querySelector('.hamb'),m=document.querySelector('.menu');if(b)b.addEventListener('click',()=>m.classList.toggle('open'));document.querySelectorAll('.menu a').forEach(a=>a.addEventListener('click',()=>m.classList.remove('open')));});
